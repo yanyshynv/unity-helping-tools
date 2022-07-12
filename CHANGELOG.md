569c874f2c545422c710a87d74500be94b49cfd1
Volodymyr Yanyshyn <yanyshynv@gmail.com>
2022-07-12 16:24:32 +0300

    added animation extension

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 # Changelog
+## [0.0.2] - 2022-07-12
+added animation extensions.
 
-## [0.0.1] - 2022-07-12
+## [0.0.1] - 2022-07-11
 This is the first release of package.
---
+++ Runtime/Extension/AnimationExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace UnityTools
+{
+    public static class AnimationExtensions
+    {
+        public static void Play(this Animator animator, string state, Action onEnd = null)
+        {
+
+        }
+    }
+}
+
